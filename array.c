5a388273a9a522d874f248f83e7e3516399e4621
Marcello Missiroli <prof.missiroli@gmail.com>
2014-04-07 16:15:30 +0200

    baseline

---
+++ array.c
@@ -0,0 +1,25 @@
+
+//TODO: Parametrizzare le dimensioni con il parametro N
+
+float a[6][6];
+
+//TODO: Funzione che inizializza tutti gli elementi a 0
+
+//TODO: Funzione che inizializza tutti gli elementi chiedendolo all'utente
+
+//TODO: Funzione che restituisce il massimo elemento
+
+//TODO: FUnzione che restituisce la somma dell'array
+
+//TODO: Funzione che restituisce la media dell'array
+
+//TODO: Funzione che stampa l'array
+
+int main() {
+
+//TODO: Mettere qui una chiamata alla funzione aggiunta (se possibile sensatamente)
+
+
+}
+
+
